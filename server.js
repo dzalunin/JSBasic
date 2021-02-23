78dc10a8c9d8dd8974f3d216e59041b9996046ce
d.zalunin <d.zalunin@gmail.com>
2021-02-23 18:08:07 +0300

    Buxfix

--- server.js
+++ server.js
@@ -1,40 +1,44 @@
-"Use strict";
+"Use strict"
 
 const http = require('http')
 const fs = require('fs')
+const url = require('url');
 
 const settings = {
+    "index": "index.html",
     "appName": "site",
     "static": "assets"
 }
 
+
+
 const pathOnServer = function (httpRequest) {
+    // console.log(httpRequest.url)
+    // console.log(url.parse(httpRequest.url).pathname)
+    const acceptPriority = httpRequest.headers.accept.split(';')
 
-    const acceptPriority = httpRequest.headers.accept.split(';');
-    if (acceptPriority.lenth == 0 || acceptPriority[0].indexOf('html') >= 0) {
-        const url = httpRequest.url === '/' ? ('/index.html') : (httpRequest.url)
-
-        return `./${settings.appName}${url}`
+    let path = url.parse(httpRequest.url).pathname
+    path = path === '/' ? (`/${settings.index}`) : (path)
+    if (acceptPriority.lenth == 0 || acceptPriority[0].indexOf('text/html') >= 0) {
+        return `./${settings.appName}${path}`
     }
 
-    return `./${settings.appName}/${settings.static}${httpRequest.url}`
-
+    return `./${settings.appName}/${settings.static}${path}`
 }
 
 
 const queryProsessor = function (httpRequest, httpResponse) {
-
     const path = pathOnServer(httpRequest)
     try {
         httpResponse.end(fs.readFileSync(path))
     } catch (error) {
         console.log(`File not found ${path}`)
 
-        httpResponse.writeHead(404, { 'Content-Type': 'text/plain' });
-        httpResponse.end('404 - Page not found.');
+        httpResponse.writeHead(404, { 'Content-Type': 'text/plain' })
+        httpResponse.end('404 - Page not found.')
     }
 }
 
 
-const srv = http.createServer(queryProsessor);
-srv.listen(process.env.PORT || 40000);
+const srv = http.createServer(queryProsessor)
+srv.listen(process.env.PORT || 40000)
